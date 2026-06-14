eed55f2f35370310605b200b7efe63f09b440329
Pongsapas Intaratoot <Pieta_in@hotmail.com>
2026-06-15 02:19:18 +0700

    add: font kanit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Lusitana, Kanit } from "next/font/google";
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const kanit = Kanit({
+  weight: ["400", "700"],
+  subsets: ["thai", "latin"],
+});
